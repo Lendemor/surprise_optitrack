b8fc004cd6e04534badd75297219d8f4e3e0e30a
Thomas Brandého <thomas.brandeho@gmail.com>
2016-05-03 15:28:57 +0200

    plot 3D

--- scripts/smooth_optitrack.py
+++ scripts/smooth_optitrack.py
@@ -1,23 +1,76 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Copyright (c) 2015 LAAS/CNRS
+All rights reserved.
+
+Redistribution and use  in source  and binary  forms,  with or without
+modification, are permitted provided that the following conditions are met:
+
+    1. Redistributions of source code must retain the above copyright notice,
+       this list of conditions and the following disclaimer.
+    2. Redistributions in binary form must reproduce the above copyright
+       notice, this list of conditions and the following disclaimer in the
+       documentation and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+POSSIBILITY OF SUCH DAMAGE.
+                                        Thomas BRANDÉHO on Sat Apr 26 2016
+"""
+#import time
 import rospy
-from std_msgs.msg import String
+import pylab as p
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
-def callback(data):
-    rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
-    
-def listener():
+from hanp_msgs.msg import TrackedHumans
+#from scipy.signal import savgol_filter
+#from std_msgs.msg import String
 
-    # In ROS, nodes are uniquely named. If two nodes with the same
-    # node are launched, the previous one is kicked off. The
-    # anonymous=True flag means that rospy will choose a unique
-    # name for our 'listener' node so that multiple listeners can
-    # run simultaneously.
-    rospy.init_node('listener', anonymous=True)
+class My3DPlot():
+    hasdata = False
+    time = np.array([])
+    first_stamp = 0
+    speed = {"X":np.array([]),"Y":np.array([]),"Z":np.array([])}
+    windowSize = 9
+    polyOrder = 2
 
-    rospy.Subscriber("chatter", String, callback)
+    def smooth_callback(self, data):
+        if len(data.tracks):
+            if self.time.size == 0:
+                self.first_stamp = data.header.stamp
+            dr = (data.header.stamp - self.first_stamp).to_sec()
+            self.time = np.append(self.time,dr)
+            self.speed["X"] = np.append(self.speed["X"],data.tracks[0].pose.pose.position.x)
+            self.speed["Y"] = np.append(self.speed["Y"],data.tracks[0].pose.pose.position.y)
+            self.hasdata = True
 
-    # spin() simply keeps python from exiting until this node is stopped
-    rospy.spin()
-
+    def smooth_optitrack(self):
+        rospy.init_node('smooth_optitrack', anonymous=True)
+        rospy.Subscriber("optitrack_person/tracked_persons", TrackedHumans, self.smooth_callback)
+        fig=plt.figure()
+        ax = fig.gca(projection='3d')
+        varNull = raw_input('Press enter to continue..')
+        while not rospy.is_shutdown():
+            if self.hasdata:  
+                ax.cla()
+                ax.set_xlabel('X')
+                ax.set_ylabel('Y')
+                ax.set_zlabel('time')
+                ax.plot(xs=self.speed["X"],ys=self.speed["Y"],zs=self.time,label='movement curve')
+                self.hasdata = False
+            plt.show()
+            
 if __name__ == '__main__':
-    listener()
+    My3DPlot().smooth_optitrack()
